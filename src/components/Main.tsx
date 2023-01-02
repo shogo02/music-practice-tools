d70953531fd9df96baa92cd968aff478ab714cc5
shogo02 <shogo.02222@gmail.com>
2023-01-02 20:44:51 +0900

    ScoreJS導入

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import parse from 'html-react-parser'
 import { useEffect } from 'react'
 import { useSnapshot } from 'valtio'
 import { Note } from 'webmidi'
+import abcjs from 'abcjs'
 import { GameContoller } from '../controller/GameController'
 import { gameState } from '../controller/GameState'
 import { MidiController } from '../controller/MidiController'
@@ -18,7 +19,8 @@ function Main() {
     GameContoller.initialize()
   }, [])
 
-  const { currentBeat, currentChord, correctNotes, playingNotes, selectedAccidental } = useSnapshot(gameState)
+  const { currentBeat, currentChord, correctNotes, playingNotes, selectedAccidental, selectedDiatonicRoot } =
+    useSnapshot(gameState)
 
   let tmpPlayingNotes = [...playingNotes] as Array<Note>
   let displayNortesInChord = [...currentChord.notesInChord] as Array<Note>
@@ -32,8 +34,15 @@ function Main() {
     .sort((a, b) => (a.number < b.number ? -1 : 1))
     .map((e) => e.name + (e.accidental ?? ''))
 
+  abcjs.renderAbc(
+    'abcjs',
+    `K:${selectedDiatonicRoot}
+  [${displayNortesInChord.map((e) => e.name).join('')}]2`,
+    { minPadding: 10, paddingtop: 0, paddingbottom: 0, paddingleft: 0, paddingright: 0 }
+  )
+
   return (
-    <div className="border border-black h-full bg-[#000730] text-cyan-200 p-7 flex flex-col items-center">
+    <div className="border border-black h-full bg-[#000730] text-cyan-200 p-4 flex flex-col items-center">
       <div className="text-4xl">{currentBeat}</div>
       <div className="text-6xl text-center">{parse(convertMusicalSymbols(currentChord?.chordName ?? 'X'))}</div>
       <div className="text-4xl text-center">
@@ -47,10 +56,9 @@ function Main() {
         })}
       </div>
       <div className="text-3xl text-center">{currentChord?.notesInChordDegree.join(' ')}</div>
+      <div id="abcjs" className="w-[130px]" />
       <div className="text-3xl text-center">{parse(convertMusicalSymbols(displayPlayingNotes.join(' ')))}</div>
-      <div className="bg-white">
-        <Score notesInChord={displayNortesInChord} selectedAccidental={selectedAccidental} />
-      </div>
+      {/* <Score notesInChord={displayNortesInChord} selectedAccidental={selectedAccidental} /> */}
       {/* <sub>7</sub><sup>(-5)</sup> */}
       {/* &#9837;	&#9839; */}
     </div>
